5ab3951f8a1a72c6fab920c1cad12fd937f72e30
Gyandip12202339 <12202339@students.koi.edu.au>
2024-01-03 20:36:38 +1100

    My first push

--- Form1.cs
+++ Form1.cs
@@ -52,7 +52,7 @@ namespace Library_project
 
                 if (textBox1.Text.Equals(username) && textBox2.Text.Equals(Password))
                 {
-                    MessageBox.Show("Successfail38");
+                    MessageBox.Show("Successfail366");
 
                 }
                 else
